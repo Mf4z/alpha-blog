b3730a31e7c729459e4ac76583406e52b295a95c
Marwan Mai <emef4z@gmail.com>
2024-11-18 00:01:11 +0100

    Add create functionality with redirect after success

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,16 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article]
+    # render plain: params[:article]
+    @article = Article.new(params.require(:article).permit(:title, :description)) # This is whitelisting the top level key :article, and it's containing data :title and :description
+    # render plain: @article.inspect # To view entered article
+    
+    if @article.save
+      # redirect_to article_path(@article) # Redirect show page
+      redirect_to @article # Redirect show page, more concise
+    else
+      render :new
+    end
   end
 
 end
