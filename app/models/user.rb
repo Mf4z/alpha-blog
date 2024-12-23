b0aeb607633c08ed77a1b4950532ed551d27558e
Marwan Mai <emef4z@gmail.com>
2024-12-23 22:12:21 +0100

    Add downcase email before saving in user Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase } # This will make sure that the email in lowercase before saving to the database
   has_many :articles, dependent: :destroy
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :username, presence: true, length: { minimum: 3, maximum: 25}, uniqueness: { case_sensitive: false }
