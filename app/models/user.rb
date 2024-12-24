c211d4fca51bd785cf53651a311c3ef8df1603c9
Marwan Mai <emef4z@gmail.com>
2024-12-24 01:02:59 +0100

    Add secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :username, presence: true, length: { minimum: 3, maximum: 25}, uniqueness: { case_sensitive: false }
   validates :email, presence: true, length: { maximum: 105}, uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+  has_secure_password # This will add password and password_confirmation attributes, require a password_digest attribute, require the presence of the password, require that they match, and require a minimum length
 end
