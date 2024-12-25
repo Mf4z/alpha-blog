16c27c93110fc96a79112237b4c3f5756cdbf96b
Marwan Mai <emef4z@gmail.com>
2024-12-25 23:29:49 +0100

    Add signup functionality create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    # byebug  - stop the code here and check the params
+    # params - to check the params
+    # prams[:user][:username] - to check the username for example
+    
+    # Create user
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Signup Successful! Welcome #{@user.username}"
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
