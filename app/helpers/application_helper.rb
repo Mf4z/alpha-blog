f70a3ea5a2dac58f21bd935c9390345f69ced13b
Marwan Mai <emef4z@gmail.com>
2024-12-27 23:43:21 +0100

    Add profile page for user and articles associated with user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def gravatar_for(user, options = { size: 80 })
+    require 'digest'
+    
+    email_address = user.email.downcase
+    hash = Digest::MD5.hexdigest(email_address)
+    size = options[:size]
+    
+    image_src = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(image_src, alt: user.username, class:"shadow rounded mx-auto d-block")
+  end
 end
