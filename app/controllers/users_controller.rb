5c895e8455534ca498e0a840cae6e86d7a9c864b
Marwan Mai <emef4z@gmail.com>
2024-12-28 22:45:51 +0100

    Add pagination to indexes and show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,8 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show, :destroy]
   
   def index
-    @users = User.all
+    # @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -14,7 +15,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    
+    @articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
 
   def create
