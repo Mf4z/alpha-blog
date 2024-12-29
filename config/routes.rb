1689eb14a57464e770709650527777fc966acca5
Marwan Mai <emef4z@gmail.com>
2024-12-29 20:35:20 +0100

    Add login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   # post 'users', to: 'users#create'
   resources :users, except: [:new]
+
+  # login routes with sessions
+  get 'login', to:'sessions#new'
+  post 'login', to:'sessions#create'
+  delete 'logout', to:'sessions#destroy'
 end
