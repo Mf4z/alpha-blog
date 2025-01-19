76ee74e764471f4e6296b17a48a257cc3a7b0b01
Marwan Mai <emef4z@gmail.com>
2025-01-19 23:55:42 +0100

    Add catgory test

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,4 +2,8 @@ require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
   
+  test "catergory should be valid" do
+    @category = Category.new(name: "News")
+    assert @category.valid?
+  end
 end
