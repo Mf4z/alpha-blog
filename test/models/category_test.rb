dfacaf9c17ea46681ab967922b8549e8419d456a
Marwan Mai <emef4z@gmail.com>
2025-01-20 23:52:35 +0100

    Add validation unit test

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -2,8 +2,28 @@ require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
   
-  test "catergory should be valid" do
-    @category = Category.new(name: "News")
+  def setup
+    @category = Category.new(name: "Present")
+  end
+  test "category should be valid" do
     assert @category.valid?
+  end
+
+  test "name should be present" do
+    @category.name = " "
+    assert_not @category.valid?
+  end
+  test "name should be unique" do
+    @category.save
+    @category2 = Category.new(name: "Present")
+    assert_not  @category2.valid?
+  end
+  test "name should not be too short" do 
+    @category.name = "aa"
+    assert_not @category.valid?
+  end
+  test "name should not be too long" do
+    @category.name = "a"  * 31
+    assert_not @category.valid?
   end
 end
